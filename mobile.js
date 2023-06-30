fb0eadd6efa1f459a1827ab53a149f01ae00b7cc
kenneth adutwum <kwamayadu@gmail.com>
2023-06-30 18:09:00 -0200

    Fix linters error

--- mobile.js
+++ mobile.js
@@ -1,101 +1,83 @@
 const mobileDetails = [
   {
     mobileDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    featureImageMobile: "images/2nd-phase/pic_on_screen.png",
-    mobileTechnologies: ["HTML", "CSS", "JavaScript"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    mobileName: "Tonic",
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    featureImageMobile: 'images/2nd-phase/pic_on_screen.png',
+    mobileTechnologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    mobileName: 'Tonic',
   },
 
   {
     mobileDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    featureImageMobile: "images/2nd-phase/Avail.png",
-    mobileTechnologies: ["HTML", "CSS", "JavaScript"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    mobileName: "Mult-Post-Stories",
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    featureImageMobile: 'images/2nd-phase/Avail.png',
+    mobileTechnologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    mobileName: 'Mult-Post-Stories',
   },
 
   {
     mobileDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    featureImageMobile: "images/2nd-phase/gym.png",
-    mobileTechnologies: ["HTML", "CSS", "JavaScript"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    mobileName: "Tonic",
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    featureImageMobile: 'images/2nd-phase/gym.png',
+    mobileTechnologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    mobileName: 'Tonic',
   },
 
   {
     mobileDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    featureImageMobile: "images/2nd-phase/art.png",
-    mobileTechnologies: ["HTML", "CSS", "JavaScript"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    mobileName: "Multi-Post-Stories",
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    featureImageMobile: 'images/2nd-phase/art.png',
+    mobileTechnologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    mobileName: 'Multi-Post-Stories',
   },
 ];
 
-const clickProjectListener = (index) => {
-    // before show, set the modal details
-    const mobileTopic = document.querySelector(".Pop-mobile-topic"); //
-    mobileTopicopic.innerHTML = mobileDetails[index].mobileNamename; 
-  
-    const image = document.querySelector(".pop-mobile-img");
-     image.src = mobileDetails[index].featureImage; 
-  
-    const text = document.querySelector(".pop-mobile-text");
-    text.innerHTML = mobileDetails[index].description;
-  
-    // see live
-    const seeLive = document.querySelector(".see-live");
-    seeLive.addEventListener("click", () =>
-      window.open(mobileDetails[index].linkToLiveSection, "_blank")
-    );
-  
-    const seeSource = document.querySelector(".see-source");
-    seeSource.addEventListener("click", () =>
-      window.open(mobileDetails[index].linkToSource, "_blank")
-    );
-  
-    // technologies
-    const techParent = document.querySelector(".pop-tech");
-    techParent.innerHTML = "";
-    mobileDetails[index].technologies.map((td) => {
-      const list = document.createElement("li");
-      list.textContent = td;
-      list.classList = "pop-up-tech-type";
-      techParent.appendChild(list);
-    });
-    // open modal
-    document.querySelector(".project-details-modal").classList.toggle("show");
-  };
-  
+const clickListener = (index) => {
+  // before show, set the modal details
+  const mobileTopic = document.querySelector('.Pop-mobile-topic');
+  mobileTopic.innerHTML = mobileDetails[index].mobileName;
 
+  const imageMobile = document.querySelector('#mobile-popup-img');
+  imageMobile.src = mobileDetails[index].featureImageMobile;
+
+  const text = document.querySelector('.pop-mobile-text');
+  text.innerHTML = mobileDetails[index].mobileDescription;
+
+  // see live
+  const seeLive = document.querySelector('.see-live');
+  seeLive.addEventListener('click', () => window.open(mobileDetails[index].linkToLiveSection, '_blank'));
+
+  const seeSource = document.querySelector('.see-source');
+  seeSource.addEventListener('click', () => window.open(mobileDetails[index].linkToSource, '_blank'));
+
+  // technologies
+  const techParent = document.querySelector('.pop-mobile-tech');
+  techParent.innerHTML = '';
+  mobileDetails[index].mobileTechnologies.forEach((td) => {
+    const list = document.createElement('li');
+    list.textContent = td;
+    list.classList = 'pop-up-tech-type';
+    techParent.appendChild(list);
+  });
+  // open modal
+  document.querySelector('.mobile-project-modal').classList.toggle('show');
+};
 
 const mobileSeeProjectButtons = document.querySelectorAll(
-    ".see-project-mobile"
-  );
-  mobileSeeProjectButtons.forEach((mobileSeeProjectButton, index) => {
-    mobileSeeProjectButton.addEventListener("click", () =>
-      clickProjectListener(index)
-    );
-  });
-  
-  // close modal
-  const closeModal = document.querySelector(".close-modal");
-  closeModal.addEventListener("click", () =>
-    document.querySelector(".mobile-project-modal").classList.toggle("show")
-  );
+  '.see-project-mobile',
+);
+mobileSeeProjectButtons.forEach((mobileSeeProjectButton, index) => {
+  mobileSeeProjectButton.addEventListener('click', () => clickListener(index));
+});
+
+// close modal
+const closeMobileModal = document.querySelector('.close-mobile-modal');
+closeMobileModal.addEventListener('click', () => document.querySelector('.mobile-project-modal').classList.toggle('show'));
--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const closeButton = document.querySelector(".close");
-closeButton.addEventListener("click", () => {
-  document.querySelector(".desktop").classList.toggle("show");
+const closeButton = document.querySelector('.close');
+closeButton.addEventListener('click', () => {
+  document.querySelector('.desktop').classList.toggle('show');
 });
 
-const navButton = document.querySelector("#nav-button");
-navButton.addEventListener("click", () => {
-  document.querySelector(".desktop").classList.toggle("show");
+const navButton = document.querySelector('#nav-button');
+navButton.addEventListener('click', () => {
+  document.querySelector('.desktop').classList.toggle('show');
 });
 
-const linkButtons = document.querySelectorAll(".links");
+const linkButtons = document.querySelectorAll('.links');
 linkButtons.forEach((linkButton) => {
-  linkButton.addEventListener("click", () => {
-    document.querySelector(".desktop").classList.toggle("show");
+  linkButton.addEventListener('click', () => {
+    document.querySelector('.desktop').classList.toggle('show');
   });
 });
 
@@ -26,11 +26,11 @@ const projectDetails = [
     Ipsum is simply dummy text of the printing and typesetting industry.
     Lorem Ipsum han printer took a galley of type and scrambled it 
     1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-    featureImage: "images/dektop-images/nature.png",
-    technologies: ["HTML", "CSS", "JavaScript", "Ruby", "Bootstrap"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    name: "Tonic",
+    featureImage: 'images/dektop-images/nature.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    name: 'Tonic',
   },
 
   {
@@ -43,28 +43,11 @@ const projectDetails = [
     Ipsum is simply dummy text of the printing and typesetting industry.
     Lorem Ipsum han printer took a galley of type and scrambled it 
     1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-    featureImage: "images/dektop-images/art.png",
-    technologies: ["HTML", "CSS", "JavaScript", "Ruby", "Bootstrap"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    name: "Multi-Post-Stories",
-  },
-
-  {
-    description:`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-    when an unknown printer took a galley of type and scrambled it 1960s.
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br/> <br/>
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it 1960s with the releorem 
-    Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum han printer took a galley of type and scrambled it 
-    1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-    featureImage: "images/dektop-images/display-pic.png", //this is your feature image. it's for desktop
-    technologies: ["HTML", "CSS", "JavaScript", "Ruby", "Bootstrap"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    name: "Facebook 360",
+    featureImage: 'images/dektop-images/art.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    name: 'Multi-Post-Stories',
   },
 
   {
@@ -77,80 +60,74 @@ const projectDetails = [
     Ipsum is simply dummy text of the printing and typesetting industry.
     Lorem Ipsum han printer took a galley of type and scrambled it 
     1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-    featureImage: "images/dektop-images/avail.png",
-    //featImage: "images/mobile-images/avail.png"
-    technologies: ["HTML", "CSS", "JavaScript", "Ruby", "Bootstrap"],
-    linkToLiveSection: "https://kwamay.github.io/Porfolio_Setup_Project",
-    linkToSource: "https://github.com/Kwamay/Porfolio_Setup_Project",
-    name: "Uber Navigation",
+    featureImage: 'images/dektop-images/display-pic.png', // this is your feature image. it's for desktop
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    name: 'Facebook 360',
+  },
+
+  {
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled it 1960s.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br/> <br/>
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it 1960s with the releorem 
+    Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum han printer took a galley of type and scrambled it 
+    1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    featureImage: 'images/dektop-images/avail.png',
+    // featImage: "images/mobile-images/avail.png"
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/Porfolio_Setup_Project',
+    linkToSource: 'https://github.com/Kwamay/Porfolio_Setup_Project',
+    name: 'Uber Navigation',
   },
 ];
-//here this is a clickProjectListener for your desktop version.
+// here this is a clickProjectListener for your desktop version.
 const clickProjectListener = (index) => {
   // before show, set the modal details
-  const topic = document.querySelector(".Pop-up-topic"); //
-  topic.innerHTML = projectDetails[index].name; 
+  const topic = document.querySelector('.Pop-up-topic'); //
+  topic.innerHTML = projectDetails[index].name;
 
-  const image = document.querySelector(".pop-up-img");
-  //here you see the image is collected from projectDetails[index].featureImage. let's go to your featureImage
-  //as it is only for desktop it will mess your mobile pop up.
-  //my suggestion is add anoher feature image in your array of the objects. and put the value to your mobile image source link.
-  //After doing this for every elementst. you can call another clickProjectlistener function for your mobile.
-  image.src = projectDetails[index].featureImage; 
-
-  const text = document.querySelector(".pop-up-text");
+  const image = document.querySelector('.pop-up-img');
+  image.src = projectDetails[index].featureImage;
+  const text = document.querySelector('.pop-up-text');
   text.innerHTML = projectDetails[index].description;
-
   // see live
-  const seeLive = document.querySelector(".see-live");
-  seeLive.addEventListener("click", () =>
-    window.open(projectDetails[index].linkToLiveSection, "_blank")
-  );
-
-  const seeSource = document.querySelector(".see-source");
-  seeSource.addEventListener("click", () =>
-    window.open(projectDetails[index].linkToSource, "_blank")
-  );
-
+  const seeLive = document.querySelector('.pop-button');
+  seeLive.addEventListener('click', () => window.open(projectDetails[index].linkToLiveSection, '_blank'));
+  const seeSource = document.querySelector('.pop-button-source');
+  seeSource.addEventListener('click', () => window.open(projectDetails[index].linkToSource, '_blank'));
   // technologies
-  const techParent = document.querySelector(".pop-tech");
-  techParent.innerHTML = "";
-  projectDetails[index].technologies.map((td) => {
-    const list = document.createElement("li");
+  const techParent = document.querySelector('.pop-tech');
+  techParent.innerHTML = '';
+  projectDetails[index].technologies.forEach((td) => {
+    const list = document.createElement('li');
     list.textContent = td;
-    list.classList = "pop-up-tech-type";
+    list.classList = 'pop-up-tech-type';
     techParent.appendChild(list);
   });
   // open modal
-  document.querySelector(".project-details-modal").classList.toggle("show");
+  document.querySelector('.project-details-modal').classList.toggle('show');
 };
 
-const desktopButtons = document.querySelectorAll(".see-project-desktop");
+const desktopButtons = document.querySelectorAll('.see-project-desktop');
 desktopButtons.forEach((desktopButton, index) => {
-  desktopButton.addEventListener("click", () => clickProjectListener(index));
+  desktopButton.addEventListener('click', () => clickProjectListener(index));
 });
 
-// const mobileSeeProjectButtons = document.querySelectorAll(
-//   ".see-project-mobile"
-// );
-// mobileSeeProjectButtons.forEach((mobileSeeProjectButton, index) => {
-//   mobileSeeProjectButton.addEventListener("click", () =>
-//     clickProjectListener(index)
-//   );
-// });
-
 // close modal
-const closeModal = document.querySelector(".close-modal");
-closeModal.addEventListener("click", () =>
-  document.querySelector(".project-details-modal").classList.toggle("show")
-);
+const closeModal = document.querySelector('.close-modal');
+closeModal.addEventListener('click', () => document.querySelector('.project-details-modal').classList.toggle('show'));
 
 // Form Validation
-const form = document.getElementById("myForm");
-const emailInput = document.getElementById("mail");
-const errorMessage = document.getElementById("errorMessage");
+const form = document.getElementById('myForm');
+const emailInput = document.getElementById('mail');
+const errorMessage = document.getElementById('errorMessage');
 
-form.addEventListener("submit", function (event) {
+form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent form submission
 
   const emailValue = emailInput.value;
@@ -159,7 +136,7 @@ form.addEventListener("submit", function (event) {
     form.submit();
   } else {
     // Email is not in lowercase, display error message
-    errorMessage.textContent = "Email must be in lowercase.";
-    errorMessage.style.display = "block";
+    errorMessage.textContent = 'Email must be in lowercase.';
+    errorMessage.style.display = 'block';
   }
 });
